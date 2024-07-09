83d716712d91e5860d1052a7cd943a0ed1433cbe
Er-VinayKumar639 <123vinaychaurasiya@gmail.com>
2024-07-09 09:15:08 +0530

    Project Completed

---
+++ Bookstore-CourseraFinalProjectBackend/router/booksdb.js
@@ -0,0 +1,19 @@
+let books = {
+      1: {
+            "author": "Chinua Achebe",
+            "title": "Things Fall Apart", 
+            "reviews": {},
+            "isbn": "1",
+        },
+      2: {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": {review1:"this is very intersting story "}, "isbn": "2"},
+      3: {"author": "Dante Alighieri","title": "The Divine Comedy", "reviews": {}, "isbn": "3"},
+      4: {"author": "Unknown","title": "The Epic Of Gilgamesh", "reviews": {}, "isbn": "4"},
+      5: {"author": "Unknown","title": "The Book Of Job", "reviews": {}, "isbn": "5"},
+      6: {"author": "Unknown","title": "One Thousand and One Nights", "reviews": {}, "isbn": "6"},
+      7: {"author": "Unknown","title": "Nj\u00e1l's Saga", "reviews": {}, "isbn": "7"},
+      8: {"author": "Jane Austen","title": "Pride and Prejudice", "reviews": {}, "isbn": "8"},
+      9: {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "reviews": {}, "isbn": "9"},
+      10: {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": {}, "isbn": "10"},
+}
+
+module.exports=books;
